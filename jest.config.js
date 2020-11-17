e3ffeb573d8990240e5606b2fd8adc56284f244c
TimothyAkana <TimothyAkana@gmail.com>
2020-11-17 15:06:33 -0800

    Saving a known configuration of working webpack before switching to v4

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
   // cacheDirectory: "/private/var/folders/hl/_q3fc8451v7c_131lsvl_02r0000gn/T/jest_dx",
 
   // Automatically clear mock calls and instances between every test
-  // clearMocks: false,
+  clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
