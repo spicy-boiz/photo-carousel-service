648d202c96801d1641f969563894d1feda611eaa
TimothyAkana <TimothyAkana@gmail.com>
2020-11-20 21:43:01 -0800

    Set up server to be usable from Proxy server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/extensions
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('photoCarousel'));
+ReactDOM.render(<App />, document.getElementById('service1'));
